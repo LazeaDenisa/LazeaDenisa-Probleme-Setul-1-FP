187d619e6d47c4625586402f04f885b258c35efd
davidpopescu <davidpopescu2002@gmail.com>
2021-12-22 21:10:33 +0200

    dasdsada

---
+++ Ex.12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinati cate numere integi divizibile cu n se afla in intervalul [a, b].asdsadadsaa
+        }
+    }
+}
